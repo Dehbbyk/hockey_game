9bd2ed19f3552403405dbd3d3b051e100ba158ad
SSID98 <joelemekaachi@gmail.com>
2024-08-07 01:24:21 +0100

    puck initial movement and positin fix

--- lib/components/puck.dart
+++ lib/components/puck.dart
@@ -1,34 +1,33 @@
-
-
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
-import 'package:hockey_game/components/paddle.dart';
 import 'package:hockey_game/UI/game.dart';
+import 'package:hockey_game/components/paddle.dart';
 
-class Puck extends CircleComponent with HasGameRef<GlowHockeyGame>,CollisionCallbacks {
-  Puck() : super(radius: 10, paint: Paint()
-    ..color = Colors.white) {
+class Puck extends CircleComponent
+    with HasGameRef<GlowHockeyGame>, CollisionCallbacks {
+  Puck() : super(radius: 13, paint: Paint()..color = Colors.white) {
     add(CircleHitbox());
   }
 
   Vector2 velocity = Vector2.zero(); // Start with zero velocity
-  bool isInitialHit = false;// Initial velocity of the puck
+  bool isInitialHit = false; // Initial velocity of the puck
 
   @override
   void onGameResize(Vector2 gameSize) {
     super.onGameResize(gameSize);
-    position = gameSize /
-        2; // Position the puck at the center once the game size is known
+    position = Vector2(
+        gameSize.x / 2.1,
+        gameSize.y / 2 -
+            radius); // Position the puck at the center once the game size is known
   }
-
 
   @override
   void update(double dt) {
     super.update(dt);
     position += velocity * dt;
 
-    // Check for collisions with the edges of the screen
+    // Bounce off the left and right edges
     if (position.x - radius < 0) {
       position.x = radius;
       velocity.x = -velocity.x;
@@ -37,6 +36,7 @@ class Puck extends CircleComponent with HasGameRef<GlowHockeyGame>,CollisionCall
       velocity.x = -velocity.x;
     }
 
+    // Bounce off the top and bottom edges
     if (position.y - radius < 0) {
       position.y = radius;
       velocity.y = -velocity.y;
