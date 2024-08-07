ed85c9db465c1b3cdbd8d25eda32f9c7e4cf936f
damilare-d <damilareakinwale0@gmail.com>
2024-08-07 07:17:20 +0100

    added assets for the puck, paddle and the board, remains background

--- lib/components/paddle.dart
+++ lib/components/paddle.dart
@@ -1,23 +1,85 @@
+//
+// import 'dart:ui';
+//
+// import 'package:flame/collisions.dart';
+// import 'package:flame/components.dart';
+// import 'package:flame/events.dart';
+// import 'package:hockey_game/components/puck.dart';
+//
+//
+// class Paddle extends PositionComponent with DragCallbacks {
+//   final Color color;
+//   final double minX;
+//   final double maxX;
+//   final double minY;
+//   final double maxY;
+//   final Puck puck;
+//   late Paint paint; // Define paint here
+//
+//   Paddle({
+//     required this.color,
+//     required this.minX,
+//     required this.maxX,
+//     required this.minY,
+//     required this.maxY,
+//     required Vector2 position,
+//     Vector2? size,
+//     required this.puck,
+//   }) : super(
+//     position: position,
+//     size: size ?? Vector2(20, 100), // Default size of the paddle
+//     anchor: Anchor.center,
+//   ) {
+//     paint = Paint()..color = color; // Initialize paint with the paddle color
+//     add(RectangleHitbox()); // Add hitbox for collision detection
+//   }
+//
+//   Vector2 velocity = Vector2.zero();
+//
+//   @override
+//   void render(Canvas canvas) {
+//     super.render(canvas);
+//     canvas.drawRect(size.toRect(), paint); // Use the paint object for drawing
+//   }
+//
+//   @override
+//   void onDragUpdate(DragUpdateEvent event) {
+//     // Move the paddle within the allowed vertical range
+//     Vector2 newPosition = position + event.canvasDelta;
+//     // Clamp the position within the allowed range
+//     newPosition.x = newPosition.x.clamp(minX, maxX);
+//     newPosition.y = newPosition.y.clamp(minY, maxY);
+//     velocity = event.canvasDelta;
+//     position = newPosition;
+//
+//     // Check for collision with puck
+//     if (puck.containsPoint(position + size / 2)) {
+//       puck.velocity = velocity.normalized() * puck.velocity.length;
+//     }
+//   }
+//
+//   @override
+//   void onDragEnd(DragEndEvent event) {
+//     // Reset the velocity when the drag ends
+//     velocity = Vector2.zero();
+//   }
+// }
 
 import 'dart:ui';
-
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:hockey_game/components/puck.dart';
 
-
-class Paddle extends PositionComponent with DragCallbacks {
-  final Color color;
+class Paddle extends SpriteComponent with DragCallbacks, CollisionCallbacks {
   final double minX;
   final double maxX;
   final double minY;
   final double maxY;
   final Puck puck;
-  late Paint paint; // Define paint here
 
   Paddle({
-    required this.color,
+    required Sprite sprite,
     required this.minX,
     required this.maxX,
     required this.minY,
@@ -26,21 +88,15 @@ class Paddle extends PositionComponent with DragCallbacks {
     Vector2? size,
     required this.puck,
   }) : super(
+    sprite: sprite,
     position: position,
     size: size ?? Vector2(20, 100), // Default size of the paddle
     anchor: Anchor.center,
   ) {
-    paint = Paint()..color = color; // Initialize paint with the paddle color
     add(RectangleHitbox()); // Add hitbox for collision detection
   }
 
   Vector2 velocity = Vector2.zero();
-
-  @override
-  void render(Canvas canvas) {
-    super.render(canvas);
-    canvas.drawRect(size.toRect(), paint); // Use the paint object for drawing
-  }
 
   @override
   void onDragUpdate(DragUpdateEvent event) {
--- lib/components/puck.dart
+++ lib/components/puck.dart
@@ -1,4 +1,60 @@
-
+//
+//
+// import 'package:flame/collisions.dart';
+// import 'package:flame/components.dart';
+// import 'package:flutter/material.dart';
+// import 'package:hockey_game/components/paddle.dart';
+// import 'package:hockey_game/UI/game.dart';
+//
+// class Puck extends CircleComponent with HasGameRef<GlowHockeyGame>,CollisionCallbacks {
+//   Puck() : super(radius: 10, paint: Paint()
+//     ..color = Colors.white) {
+//     add(CircleHitbox());
+//   }
+//
+//   Vector2 velocity = Vector2.zero(); // Start with zero velocity
+//   bool isInitialHit = false;// Initial velocity of the puck
+//
+//   @override
+//   void onGameResize(Vector2 gameSize) {
+//     super.onGameResize(gameSize);
+//     position = gameSize /
+//         2; // Position the puck at the center once the game size is known
+//   }
+//
+//
+//   @override
+//   void update(double dt) {
+//     super.update(dt);
+//     position += velocity * dt;
+//
+// //   Bounce off the left and right edges
+//     if (position.x <= 0 || position.x >= gameRef.size.x) {
+//       velocity.x = -velocity.x;
+//     }
+//     // Bounce off the top and bottom edges
+//     if (position.y <= 0 || position.y >= gameRef.size.y) {
+//       velocity.y = -velocity.y;
+//     }
+//   }
+//
+//   @override
+//   void onCollision(Set<Vector2> intersectionPoints, PositionComponent other) {
+//     super.onCollision(intersectionPoints, other);
+//     if (other is Paddle) {
+//       if (!isInitialHit) {
+//         // Set initial velocity on first hit
+//         velocity = Vector2(200, 200);
+//         isInitialHit = true;
+//       } else {
+//         // Reverse the direction of the puck on collision with a paddle
+//         velocity.y = -velocity.y;
+//         // Add the paddle's velocity to the puck's velocity for more realistic interaction
+//         velocity += other.velocity * 0.3;
+//       }
+//     }
+//   }
+// }
 
 import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
@@ -6,29 +62,30 @@ import 'package:flutter/material.dart';
 import 'package:hockey_game/components/paddle.dart';
 import 'package:hockey_game/UI/game.dart';
 
-class Puck extends CircleComponent with HasGameRef<GlowHockeyGame>,CollisionCallbacks {
-  Puck() : super(radius: 10, paint: Paint()
-    ..color = Colors.white) {
+class Puck extends SpriteComponent with HasGameRef<GlowHockeyGame>, CollisionCallbacks {
+  Vector2 velocity = Vector2.zero(); // Start with zero velocity
+  bool isInitialHit = false; // Initial velocity of the puck
+
+  Puck({required Sprite sprite}) : super(
+    sprite: sprite,
+    size: Vector2(20, 20),
+    anchor: Anchor.center,
+  ) {
     add(CircleHitbox());
   }
-
-  Vector2 velocity = Vector2.zero(); // Start with zero velocity
-  bool isInitialHit = false;// Initial velocity of the puck
 
   @override
   void onGameResize(Vector2 gameSize) {
     super.onGameResize(gameSize);
-    position = gameSize /
-        2; // Position the puck at the center once the game size is known
+    position = gameSize / 2; // Position the puck at the center once the game size is known
   }
-
 
   @override
   void update(double dt) {
     super.update(dt);
     position += velocity * dt;
 
-//   Bounce off the left and right edges
+    // Bounce off the left and right edges
     if (position.x <= 0 || position.x >= gameRef.size.x) {
       velocity.x = -velocity.x;
     }
