9ba94118ad0dddcb504fb1265da3fc82c4547048
damilare-d <damilareakinwale0@gmail.com>
2024-08-07 18:55:59 +0100

    added game engine for scores calc

---
+++ lib/components/game_engine.dart
@@ -0,0 +1,17 @@
+
+
+import 'package:flame/components.dart';
+
+class GameEngine extends Component {
+  int player1Score = 0;
+  int player2Score = 0;
+
+  void scoreGoal(int player) {
+    if (player == 1) {
+      player1Score++;
+    } else if (player == 2) {
+      player2Score++;
+    }
+    print('Player $player scored! Score: $player1Score - $player2Score');
+  }
+}
